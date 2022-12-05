a504afe5228346fa0acf31ee78268f8d25febc99
FinkParker <parkeririshgod@gmail.com>
2022-12-04 20:34:51 -0500

    Basic Node Setup

---
+++ Text Processor/main.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+app.use(express.urlencoded({extended:true}))
+app.set('view engine', 'ejs')
+const port = 1000
+const ip = '127.0.0.1'
+
+
+app.get('/', function(req,res){
+  res.render('index.ejs')  
+})
+
+app.listen(port, ip, function(){
+    console.log("Server Up...")
+})
