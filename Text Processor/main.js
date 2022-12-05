4159c38b63a0c1f2598fd86df60279c0a1ba6c67
FinkParker <parkeririshgod@gmail.com>
2022-12-04 21:04:18 -0500

    basic form input

--- Text Processor/main.js
+++ Text Processor/main.js
@@ -10,6 +10,21 @@ app.get('/', function(req,res){
   res.render('index.ejs')  
 })
 
+app.post('/', function(req,res){
+  let txtDocument = {
+     title : req.body.title,
+     body : req.body.bodytxt
+  }
+  res.redirect('/')
+  console.log(txtDocument)
+})
+
+
+
+
+
+
+
 app.listen(port, ip, function(){
     console.log("Server Up...")
 })
