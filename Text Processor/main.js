d773fbbbaa011f4ebe1a416c653f3517cc04f70e
FinkParker <parkeririshgod@gmail.com>
2022-12-22 21:35:37 -0500

    Restart

--- Text Processor/main.js
+++ Text Processor/main.js
@@ -1,40 +1,22 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
-app.use(express.urlencoded({extended:true}))
 app.set('view engine', 'ejs')
-const port = 1000
-const ip = '127.0.0.1'
+app.use(express.urlencoded({extended: true}))
 
 
 
-
-
-
-//home
 app.get('/', function(req,res){
-  let main = fs.readdirSync('Documents/')
-  res.render('index.ejs',{
-    files: main
-  })
-  console.log(main)
+    res.render('index.ejs')
 })
 
 app.post('/', function(req,res){
-  let txtDocument = {
-     title : req.body.title,
-     body : req.body.bodytxt
-  }
-  res.redirect('/')
-  console.log(txtDocument)
-  let stringData = JSON.stringify(txtDocument)
-  console.log(stringData)
-  fs.writeFileSync("Documents/" + txtDocument.title + '.txt', txtDocument.body)
+    res.render('index.ejs')
+    
 })
 
 
 
-
-app.listen(port, ip, function(){
-    console.log("Server Up...")
+app.listen(1000, '127.0.0.1', function(){
+    console.log("Server: Functional ")
 })
